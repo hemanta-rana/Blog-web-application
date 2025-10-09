5acbfcfe1a7f5e39e58e4d3e81a48ab5be14ebbd
Hemanta Rana <magarhemanta145@gmail.com>
2025-10-09 18:12:28 +0545

    add tag entity

---
+++ Blog/src/main/java/com/admish/blog/domain/entities/Tag.java
@@ -0,0 +1,36 @@
+package com.admish.blog.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(id, tag.id) && Objects.equals(name, tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
