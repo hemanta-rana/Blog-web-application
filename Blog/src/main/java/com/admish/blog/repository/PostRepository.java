49136cd815f8366a11334e0144468fcd9186c61e
Hemanta Rana <magarhemanta145@gmail.com>
2025-10-10 13:19:05 +0545

    add post repo

---
+++ Blog/src/main/java/com/admish/blog/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.admish.blog.repository;
+
+import com.admish.blog.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
