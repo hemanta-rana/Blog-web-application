6f7af6dd2d85c4e9ba92b9e85e8fa9440b354ffd
Hemanta Rana <magarhemanta145@gmail.com>
2025-10-10 13:17:52 +0545

    add tag and category repository

---
+++ Blog/src/main/java/com/admish/blog/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.admish.blog.repository;
+
+import com.admish.blog.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+}
