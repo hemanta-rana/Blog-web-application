0d9ee26ef1e0d59142532a8e7c87cab765b72ab0
Hemanta Rana <magarhemanta145@gmail.com>
2025-10-11 11:54:32 +0545

    imlement category mapper

--- Blog/src/main/java/com/admish/blog/mapper/CategoryMapper.java
+++ Blog/src/main/java/com/admish/blog/mapper/CategoryMapper.java
@@ -1,4 +1,29 @@
 package com.admish.blog.mapper;
 
+import com.admish.blog.domain.PostStatus;
+import com.admish.blog.domain.dtos.CategoryDto;
+import com.admish.blog.domain.entities.Category;
+import com.admish.blog.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryMapper {
+    @Mapping(target = "postCount",source = "posts", qualifiedByName = "calculatePostCount")
+    CategoryDto toDto(Category category);
+
+    @Named("calculatePostCount")
+    default long calculatePostCount(List<Post> posts){
+        if (null == posts){
+            return 0;
+        }
+
+        return posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
 }
