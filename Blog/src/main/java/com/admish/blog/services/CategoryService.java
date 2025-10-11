8f0edc579976c6d44a52b0ca938e27deb240df08
Hemanta Rana <magarhemanta145@gmail.com>
2025-10-11 11:55:16 +0545

    add category work

--- Blog/src/main/java/com/admish/blog/services/CategoryService.java
+++ Blog/src/main/java/com/admish/blog/services/CategoryService.java
@@ -1,4 +1,10 @@
 package com.admish.blog.services;
 
+
+import com.admish.blog.domain.entities.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> listCategories();
 }
--- Blog/src/main/java/com/admish/blog/services/impl/CategoryServiceImpl.java
+++ Blog/src/main/java/com/admish/blog/services/impl/CategoryServiceImpl.java
@@ -1,4 +1,22 @@
 package com.admish.blog.services.impl;
 
-public class CategoryServiceImpl {
+
+import com.admish.blog.domain.entities.Category;
+import com.admish.blog.repository.CategoryRepository;
+import com.admish.blog.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> listCategories() {
+        return categoryRepository.findAllWithPostCount();
+    }
 }
